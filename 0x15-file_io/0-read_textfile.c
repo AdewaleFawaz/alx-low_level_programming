4b9a8158ddde413e0a2f525104545ca0d9e7a9db
AdewaleFawaz <fawazadewale120@gmail.com>
2024-02-16 11:57:13 -0800

    add

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,40 +2,28 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @filename: Text file being read
- * @letters: Number of letters to be read
- * Return: w - Actual number of bytes read and printed
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
  *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buffer;
-    ssize_t file_descriptor;
-    ssize_t written_bytes;
-    ssize_t total_bytes;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    // Open the file for reading
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    // Allocate memory for a buffer to store the content of the file
-    buffer = malloc(sizeof(char) * letters);
-
-    // Read content from the file into the buffer
-    total_bytes = read(file_descriptor, buffer, letters);
-
-    // Write the content of the buffer to STDOUT
-    written_bytes = write(STDOUT_FILENO, buffer, total_bytes);
-
-    // Free the allocated buffer
-    free(buffer);
-
-    // Close the file
-    close(file_descriptor);
-
-    // Return the actual number of bytes read and printed
-    return (written_bytes);
+	free(buf);
+	close(fd);
+	return (w);
 }
 
