bfe49f291f1706b417fe6d10685f6850807af09e
AdewaleFawaz <fawazadewale120@gmail.com>
2024-02-16 12:09:22 -0800

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,22 +17,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		len = strlen(text_content);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
-	if (w == -1)
-	{
-		close(fd);
+
+	if (fd == -1 || w == -1)
 		return (-1);
-	}
 
 	close(fd);
 
 	return (1);
 }
+
 
