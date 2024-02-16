463d40e63f1accd19e5226c44f8c5c10d18436de
AdewaleFawaz <fawazadewale120@gmail.com>
2024-02-16 12:07:54 -0800

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file with specified permissions and writes content.
- * @filename: Name of the file to create.
- * @text_content: NULL terminated string to write to the file.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, write_status, close_status;
-    mode_t permissions = S_IRUSR | S_IWUSR; // rw-------
+	int fd, w, len = 0;
 
-    if (filename == NULL)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permissions);
-    if (fd == -1)
-        return -1;
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+	}
 
-    if (text_content != NULL)
-    {
-        write_status = write(fd, text_content, strlen(text_content));
-        if (write_status == -1)
-        {
-            close(fd);
-            return -1;
-        }
-    }
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    close_status = close(fd);
-    if (close_status == -1)
-        return -1;
+	w = write(fd, text_content, len);
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 
-    return 1;
+	close(fd);
+
+	return (1);
 }
 
